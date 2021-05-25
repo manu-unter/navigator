8823688a071c60cf8629b9b423597075cbb71251
manu-unter <manuel.unterhofer@hey.com>
2021-05-26 00:20:45 +0200

    First ugly but working version of a file system walker and lazy list

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,46 @@
 import androidx.compose.desktop.Window
-import androidx.compose.material.Text
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
+import java.io.File
 
 fun main() = Window {
-    var text by remember { mutableStateOf("Hello, World!") }
+    var path by remember { mutableStateOf<String>("")}
+    val fileSystemWalkerScope = rememberCoroutineScope { Dispatchers.IO }
+    var currentFileWalkerJob by remember { mutableStateOf<Job?>(null)}
+    var fileTreeWalk by remember { mutableStateOf<FileTreeWalk?>(null)}
+
+    fun readFileTree(path: String) {
+        currentFileWalkerJob?.cancel()
+        currentFileWalkerJob = fileSystemWalkerScope.async {
+            println(coroutineContext)
+            fileTreeWalk = File(path).walk()
+        }
+    }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Row {
+            TextField(path, onValueChange = {
+                path = it
+                readFileTree(it)
+            })
+        }
+        Row {
+            if (fileTreeWalk != null) {
+                LazyColumn {
+                    fileTreeWalk?.forEach {
+                        item {
+                            Row {
+                                Text(it.name)
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
