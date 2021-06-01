9b87737dd5a1252662a4986ebc3352caea61ad2c
manu-unter <manuel.unterhofer@hey.com>
2021-06-01 11:08:53 +0200

    Select root node by default

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -14,7 +14,7 @@ import java.io.File
 fun main() = Window(title = "Navigator") {
     var rootPath by remember { mutableStateOf("C:\\Users\\mane\\Downloads") }
     val rootViewNode by derivedStateOf { ViewNode(Node(File(rootPath))) }
-    val selectionState = remember { mutableStateOf<ViewNode?>(null) }
+    val selectionState = remember { mutableStateOf<ViewNode?>(rootViewNode) }
 
     ApplicationTheme {
         Row(Modifier.fillMaxSize().background(color = MaterialTheme.colors.background)) {
