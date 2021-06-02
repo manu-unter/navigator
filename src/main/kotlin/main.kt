01113570580ceb87c3b316eb1a5fde185cea6eb9
manu-unter <manuel.unterhofer@hey.com>
2021-06-02 11:29:39 +0200

    Show message for non-existent paths

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,6 @@
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
@@ -26,26 +25,28 @@ fun main() = application {
             }
         }
 
-        var rootPath by remember {
-            mutableStateOf(
-                System.getProperty("user.home") ?: System.getenv("SystemDrive") ?: "/"
-            )
-        }
-        val rootViewNode by derivedStateOf { ViewNode(Node(File(rootPath))) }
+        var rootPath by remember { mutableStateOf(getInitialRootPath()) }
+        val rootViewNode by remember { derivedStateOf { getValidRootViewNode(rootPath) } }
         val selectionState = remember { mutableStateOf<ViewNode?>(rootViewNode) }
 
         ApplicationTheme {
             Row(Modifier.fillMaxSize().background(color = MaterialTheme.colors.background)) {
 
                 Surface(Modifier.weight(1f)) {
-                    Column {
+                    Column(Modifier.fillMaxSize()) {
                         TextField(
                             value = rootPath,
                             onValueChange = { rootPath = it },
                             modifier = Modifier.fillMaxWidth(),
                         )
                         Spacer(Modifier.height(2.dp))
-                        DirectoryTree(rootViewNode, selectionState)
+                        if (rootViewNode != null) {
+                            DirectoryTree(rootViewNode!!, selectionState)
+                        } else {
+                            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                                Text("Please provide a valid path", Modifier.padding(24.dp, 0.dp))
+                            }
+                        }
                     }
                 }
                 Preview(
@@ -65,4 +66,18 @@ fun getWindowIcon(): BufferedImage? {
     } catch (exception: Exception) {
         null
     }
+}
+
+fun getInitialRootPath(): String {
+    return System.getProperty("user.home") ?: System.getenv("SystemDrive") ?: "/"
+}
+
+fun getValidRootViewNode(path: String): ViewNode? {
+    val file = File(path)
+
+    if (!file.exists()) {
+        return null
+    }
+
+    return ViewNode(Node(file.canonicalFile))
 }
